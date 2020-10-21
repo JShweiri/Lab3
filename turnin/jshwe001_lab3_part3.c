7314fb21182392572f43b2ce49707477af7570f6
JShweiri <wafflemanz3000@gmail.com>
2020-10-20 20:57:18 -0700

    pts 3 and 4

---
+++ turnin/jshwe001_lab3_part3.c
@@ -0,0 +1,76 @@
+/*	Author: jshwe001
+ *  Partner(s) Name: Ryan Giron
+ *	Lab Section: 024
+ *	Assignment:1 Lab #2  Exercise # 1
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned char getBit(unsigned char data, unsigned char pos)
+{
+    return (data & (0x01 << pos));
+}
+
+int main(void)
+{
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    DDRC = 0xFF;
+    PORTC = 0x00;
+
+    unsigned char tmpA;
+    unsigned char tmpC;
+
+    while (1)
+    {
+        tmpC = 0;
+        tmpA = PINA;
+
+        if (tmpA >= 1 && tmpA <= 2)
+        {
+            tmpC = 32;
+        }
+
+        if (tmpA >= 3 && tmpA <= 4)
+        {
+            tmpC = 48;
+        }
+
+        if (tmpA >= 5 && tmpA <= 6)
+        {
+            tmpC = 56;
+        }
+
+        if (tmpA >= 7 && tmpA <= 9)
+        {
+            tmpC = 60;
+        }
+
+        if (tmpA >= 10 && tmpA <= 12)
+        {
+            tmpC = 62;
+        }
+
+        if (tmpA >= 13 && tmpA <= 15)
+        {
+            tmpC = 63;
+        }
+
+        if (tmpA <= 4)
+        {
+            tmpC |= 64;
+        }
+
+        if (getBit(tmpA, 4) && getBit(tmpA, 5) && !getBit(tmpA, 6))
+        {
+            tmpC |= 128;
+        }
+
+        PORTC = tmpC;
+    }
+}
